05da6c22fdaf9164442a94786fed42b72979493f
Ashkan <159788694+Ashkanghadimii@users.noreply.github.com>
2024-11-05 21:37:56 +0100

    Update Rock-paper-scissors.js

--- Rock-paper-scissors.js
+++ Rock-paper-scissors.js
@@ -7,6 +7,51 @@ let score = JSON.parse(localStorage.getItem
 
     updateScoreElement();
     
+    let isAUtoPlaying = false;
+    let intervalId;
+
+    function autoPlay() {
+      if (!isAUtoPlaying) {
+        intervalId = setInterval(() => {
+          const playerMove = pickComputerMove();
+          playGame(playerMove);
+        },1000);
+        isAUtoPlaying = true;
+      } else {
+        clearInterval(intervalId);
+        isAUtoPlaying = false;
+
+      }
+
+      
+    }
+
+    document.querySelector('.js-rock-button')
+    .addEventListener('click', () => {
+      playGame('rock');
+    })
+
+    document.querySelector('.js-paper-button')
+    .addEventListener('click', () => {
+      playGame('paper');
+    })
+
+    document.querySelector('.js-scissors-button')
+    .addEventListener('click', () => {
+      playGame('scissors');
+    })
+
+    document.body.addEventListener('keydown', (event) => {
+      if (event.key === 'r') {
+        playGame('rock');
+      }else if (event.key === 'p') {
+        playGame('paper');
+      }else if (event.key === 's') {
+        playGame('scissors')
+      }
+
+    })
+
     function playGame(playerMove) {
       const computerMove = pickComputerMove();
 
