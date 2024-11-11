d67cfa35c789353b8f9ce5ebfa8a2df0b42f80ba
Ashkan <159788694+Ashkanghadimii@users.noreply.github.com>
2024-11-11 18:00:27 +0100

    Update cart.js

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -65,19 +65,3 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
 
   saveToStorage();
 }
-
-export function updateDeliveryOption(productId, deliveryOptionId) {
-  let matchingItem;
-
-  cart.forEach((cartItem) => {
-    if (productId === cartItem.productId) {
-      matchingItem = cartItem;
-    }
-  });
-
-  matchingItem.deliveryOptionId = deliveryOptionId;
-
-  saveToStorage();
-
-
-}
