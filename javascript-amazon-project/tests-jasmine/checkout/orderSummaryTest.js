2f5b53141805bf3a45d71711ba8a53af832760d4
Ashkan <159788694+Ashkanghadimii@users.noreply.github.com>
2024-11-12 20:43:34 +0100

    Update orderSummaryTest.js

--- javascript-amazon-project/tests-jasmine/checkout/orderSummaryTest.js
+++ javascript-amazon-project/tests-jasmine/checkout/orderSummaryTest.js
@@ -1,4 +1,5 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js'
+import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
 
 describe('test suite: renderOrderSummary', () => {
     it('displays the cart', () => {
@@ -6,6 +7,15 @@ describe('test suite: renderOrderSummary', () => {
         .innerHTML = `
         <div class="js-order-summary"></div>
         `;
+
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([{
+              productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+              quantity: 1,
+              deliveryOptionId: '1'
+            }]);
+          });
+          loadFromStorage();
     } );
 
 });
